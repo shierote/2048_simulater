7cfe45947edd3f809857406821285d819fdb84df
Taishi Eguchi <taishi2060@gmail.com>
2019-01-18 11:12:24 +0900

    回数と最大値の計測

--- simulater.rb
+++ simulater.rb
@@ -7,8 +7,10 @@ options = ARGV.getopts("", "log")
 
 list = []
 
+game_number = 100
+
 result = Benchmark.realtime do
-  100.times do
+  game_number.times do
     p "初期状態" if options["log"]
     ary = set_first_ary
     array_print(ary) if options["log"]
@@ -23,6 +25,7 @@ end
 # p "最小値：#{list.min}"
 # p "平均値：#{list.inject(0.0){|r,i| r+=i }/list.size}"
 # p "最頻値：#{list.max_by {|value| list.count(value)}}"
+p "ゲーム回数                   #{game_number}回"
 p "各ゲーム最大値のの平均値     #{list.sum {|l| l[:max]}.to_f / list.size}"
 p "各ゲームスワイプ回数の平均値 #{list.sum {|l| l[:num]}.to_f / list.size}"
 p "処理時間                     #{result}s"
